c8002636f9e143e1a15c76ada2b88a9c53e1728b
BBED07 <dongnan1996@gmail.com>
2025-02-02 13:40:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ def delete_word(english):
     return jsonify({"error": "Word not found"}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=10000)
